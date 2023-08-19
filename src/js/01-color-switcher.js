8eb526cb7c8556184854fd0520c5070975a9a613
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-08-19 20:44:27 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  const dataStart = document.querySelector('[data-start]')
+  const dataStop = document.querySelector('[data-stop]')
+
+  let timeId = null
+
+  dataStart.addEventListener('click', () => {
+    timeId = setInterval(() =>{
+       document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    dataStart.disabled = true;
+    dataStop.disabled = false;
+});
+
+dataStop.addEventListener('click', () =>{
+    clearInterval(timeId)
+    dataStart.disabled = false;
+    dataStop.disabled = true;
+});
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+  let position = 0;
+  for (let i = 1; i <= amount; i++) {
+    position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  form.reset()
 }
